fc53e21ed689a9e0aa4a1fb68906be445d1e3662
verkeerd <susan.bruggeling@student.hu.nl>
2022-04-04 10:51:37 +0200

    Added front end developed by Nick Roumimper

---
+++ front_end/huw_recommend.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, session, render_template, redirect, url_for, g
+from flask_restful import Api, Resource, reqparse
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import content_filtering
+import collaborative_filtering
+
+app = Flask(__name__)
+api = Api(app)
+
+# We define these variables to (optionally) connect to an external MongoDB
+# instance.
+envvals = ["MONGODBUSER","MONGODBPASSWORD","MONGODBSERVER"]
+dbstring = 'mongodb+srv://{0}:{1}@{2}/test?retryWrites=true&w=majority'
+
+# Since we are asked to pass a class rather than an instance of the class to the
+# add_resource method, we open the connection to the database outside of the
+# Recom class.
+load_dotenv()
+if os.getenv(envvals[0]) is not None:
+    envvals = list(map(lambda x: str(os.getenv(x)), envvals))
+    client = MongoClient(dbstring.format(*envvals))
+else:
+    client = MongoClient()
+database = client.huwebshop
+
+class Recom(Resource):
+    """ This class represents the REST API that provides the recommendations for
+    the webshop. At the moment, the API simply returns a random set of products
+    to recommend."""
+
+    def get(self, profileid, count):
+        """ This function represents the handler for GET requests coming in
+        through the API. It currently returns a random sample of products. """
+        product_ids = content_filtering.recommend_products(profileid)
+        items_remaining = count - len(product_ids)
+        if items_remaining != 0:
+            product_ids += collaborative_filtering.recommend_products_profile(product_ids, amount=items_remaining)
+        items_remaining = count - len(product_ids)
+        if items_remaining != 0:
+            product_ids += collaborative_filtering.recently_bought_products(amount=items_remaining)
+
+        return product_ids, 200
+
+# This method binds the Recom class to the REST API, to parse specifically
+# requests in the format described below.
+api.add_resource(Recom, "/<string:profileid>/<int:count>")
