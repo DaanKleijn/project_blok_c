3a80a4c7b6a7c782fad07542a8d54c48f95cf948
verkeerd <susan.bruggeling@student.hu.nl>
2022-04-08 17:15:49 +0200

    filters out given product_id when fetching similar products

--- load_data_sql.py
+++ load_data_sql.py
@@ -19,9 +19,12 @@ def products_trending(date):
 
 
 def product_gender():
-    sql = f'SELECT product_id FROM products WHERE gender = (SELECT gender FROM products WHERE product_id = %s)'
+    sql = f'SELECT product__id FROM products WHERE gender = (SELECT gender FROM products WHERE product__id = %s) AND ' \
+          f'product__id != %s;'
     return sql
 
+
 def product_price():
-    sql = f'SELECT selling_price FROM products WHERE selling_price = (SELECT selling_price FROM products WHERE product_id = %s)'
+    sql = f'SELECT selling_price FROM products WHERE selling_price = (SELECT selling_price FROM products WHERE ' \
+          f'product__id = %s) AND product__id != %s;'
     return sql
