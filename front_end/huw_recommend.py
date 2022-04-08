ad9f02375bb2e0230c30b57e14927178b2f492dd
verkeerd <susan.bruggeling@student.hu.nl>
2022-04-08 16:28:28 +0200

    changed variables given to the recommendation functions

--- front_end/huw_recommend.py
+++ front_end/huw_recommend.py
@@ -3,8 +3,7 @@ from flask_restful import Api, Resource, reqparse
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import content_filtering
-import collaborative_filtering
+import bought_together
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,23 +24,20 @@ else:
     client = MongoClient()
 database = client.huwebshop
 
+
 class Recom(Resource):
     """ This class represents the REST API that provides the recommendations for
     the webshop. At the moment, the API simply returns a random set of products
     to recommend."""
 
-    def get(self, profileid, count):
+    def get(self, profileid, productid, page_type, count):
         """ This function represents the handler for GET requests coming in
         through the API. It currently returns a random sample of products. """
-        product_ids = content_filtering.recommend_products(profileid)
-        items_remaining = count - len(product_ids)
-        if items_remaining != 0:
-            product_ids += collaborative_filtering.recommend_products_profile(product_ids, amount=items_remaining)
-        items_remaining = count - len(product_ids)
-        if items_remaining != 0:
-            product_ids += collaborative_filtering.recently_bought_products(amount=items_remaining)
+        print(self.dispatch_request())
+        product_ids = list()
 
         return product_ids, 200
+
 
 # This method binds the Recom class to the REST API, to parse specifically
 # requests in the format described below.
