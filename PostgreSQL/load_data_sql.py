da97d784097c712be2d7f16ffa60c16942cc802e
verkeerd <susan.bruggeling@student.hu.nl>
2022-04-10 05:23:39 +0200

    deleted redundant queries

--- PostgreSQL/load_data_sql.py
+++ PostgreSQL/load_data_sql.py
@@ -10,18 +10,6 @@ def profile_id_buid_query():
     return """SELECT profile__id FROM buids WHERE buid = '{}'"""
 
 
-def product_traffic_month(month):
-    """"""
-    return """SELECT ep.product__id FROM event_products ep, sessions s 
-    WHERE EXTRACT(MONTH FROM s.session_end) = %s and ep.session__id = s.session__id;"""
-
-
-def products_trending(date):
-    """"""
-    return """SELECT product__id FROM event_products ep, sessions s 
-    WHERE ep.session__id = s.session__id AND CAST(s.session_end AS DATE) = %s"""
-
-
 def product_gender():
     sql = f'SELECT product__id FROM products WHERE gender = (SELECT gender FROM products WHERE product__id = %s) AND ' \
           f'product__id != %s;'
@@ -29,7 +17,7 @@ def product_gender():
 
 
 def product_price():
-    sql = f'SELECT selling_price FROM products WHERE selling_price = (SELECT selling_price FROM products WHERE ' \
+    sql = f'SELECT product__id FROM products WHERE selling_price = (SELECT selling_price FROM products WHERE ' \
           f'product__id = %s) AND product__id != %s;'
     return sql
 
