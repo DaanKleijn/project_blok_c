c670481b1c1eb313e37b8cfbd6d44e3768307918
verkeerd <susan.bruggeling@student.hu.nl>
2022-04-11 23:45:17 +0200

    added function to get items in a dict with lowest count (e.g. with a frequency dict)

--- recommendation_engines/statistics.py
+++ recommendation_engines/statistics.py
@@ -14,8 +14,8 @@ def get_frequency(a_list):
 
 def highest_counts(frequency_dict, amount):
     """
-    Takes a dictionary with frequencies (dict) ({item: frequency_count}) and an amount (int) as input.
-    Returns the specified amount of items that have the highest frequency counts.
+    Takes a dictionary with count (dict) ({item: frequency_count}) and an amount (int) as input.
+    Returns the specified amount of items that have the highest count.
     """
     product_amount = len(frequency_dict)
     most_frequent_items = list()
@@ -29,6 +29,31 @@ def highest_counts(frequency_dict, amount):
     while amount:
         highest_count = max(counts)
         index = counts.index(highest_count)
+        most_frequent_items.append(product_counts_list[index][0])
+        del counts[index]
+        del product_counts_list[index]
+        amount -= 1
+
+    return most_frequent_items
+
+
+def lowest_counts(frequency_dict, amount):
+    """
+    Takes a dictionary with counts (dict) ({item: frequency_count}) and an amount (int) as input.
+    Returns the specified amount of items that have the lowest count.
+    """
+    product_amount = len(frequency_dict)
+    most_frequent_items = list()
+
+    product_counts_list = list(frequency_dict.items())
+    counts = [item[1] for item in product_counts_list]
+
+    if amount > product_amount:
+        amount = product_amount
+
+    while amount:
+        lowest_count = min(counts)
+        index = counts.index(lowest_count)
         most_frequent_items.append(product_counts_list[index][0])
         del counts[index]
         del product_counts_list[index]
