d64ee33e4c272830ece194ede642df4b71158389
verkeerd <susan.bruggeling@student.hu.nl>
2022-04-12 18:43:38 +0200

    functions used with composing validation sets

--- Validation/data_conversion.py
+++ Validation/data_conversion.py
@@ -1,17 +1,53 @@
-def wanted_valuation_values_query():
-    """"""
+import json
+
+
+def valuation_values_formatted_query(amount_of_values):
     return """SELECT product__id, product_name, category, sub_category, sub_sub_category, sub_sub_sub_category 
     FROM products 
-    WHERE product_id IN ({});"""
+    WHERE product__id IN ({});""".format(','.join(['%s'] * amount_of_values))
 
 
 def get_validation_dict(products, sql_cursor):
     """"""
-    query = wanted_valuation_values_query()
+    if not products:
+        return list()
+    query = valuation_values_formatted_query(len(products))
     sql_cursor.execute(query, products)
+
     return [{'product_id': product_info[0],
       'name': product_info[1],
       'category': product_info[2],
       'sub_category': product_info[3],
       'sub_sub_category': product_info[4],
-      'sub_sub_sub_category': product_info[5] } for product_info in sql_cursor.fetchall()]
+      'sub_sub_sub_category': product_info[5]} for product_info in sql_cursor.fetchall()]
+
+
+def samples_product_dict(sql_cursor, products, json_file):
+    """"""
+    products_with_data = {}
+
+    for mother_product, recommendations in iter(products.items()):
+        products_with_data[mother_product] = {'data':
+                                                  get_validation_dict((mother_product,), sql_cursor),
+                                             'recommendations':
+                                                 get_validation_dict(tuple(recommendations), sql_cursor)}
+
+    save_samples(products_with_data, json_file)
+    return products_with_data
+
+
+def samples_other_dict(sql_cursor, products, json_file):
+    """"""
+    products_with_data = {}
+
+    for mother_product, recommendations in iter(products.items()):
+        products_with_data[mother_product] = get_validation_dict(tuple(recommendations), sql_cursor)
+    save_samples(products_with_data, json_file)
+
+    return products_with_data
+
+
+def save_samples(sample_data, json_file):
+    """"""
+    with open(json_file, 'w+') as json_open:
+        json.dump(sample_data, json_open, indent=4)
---
+++ Validation/date_helper.py
@@ -0,0 +1,46 @@
+import random
+import time
+from datetime import datetime
+
+
+# source str_time_prop and random_date: Boris V; Tom Alsberg,
+# https://stackoverflow.com/questions/553303/generate-a-random-date-between-two-other-dates
+def str_time_prop(start, end, time_format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formatted in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    source str_time_prop and random_date: Boris V; Tom Alsberg,
+    https://stackoverflow.com/questions/553303/generate-a-random-date-between-two-other-dates
+    """
+
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(time_format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    """Takes two dates and a random float as input. Returns a random date between the two dates."""
+    return str_time_prop(start, end, '%Y-%m-%d', prop)
+
+
+def get_random_dates(start, end, amount):
+    """"""
+    dates = []
+    while amount != 0:
+        a_random_date = random_date(start, end, random.random())
+        date_time_obj = datetime.strptime(a_random_date, '%Y-%m-%d')
+        if not date_time_obj in dates:
+            dates.append(date_time_obj)
+            amount -= 1
+    print(dates)
+    return dates
+
+
+if __name__ == '__main__':
+    print(random_date("2008-1-1", "2009-1-1", random.random()))
